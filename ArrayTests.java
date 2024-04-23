20ce8c5895c62b1e5e4d715913497f69f8b9ea71
伍世杰 <wushijie@wushijiedeMacBook-Pro.local>
2024-04-23 11:58:43 -0700

    Some concise message describing the change

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1,3,2 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{2,3,1 }, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {2,3,2 };
+    assertArrayEquals(new int[]{ 2,3,2}, ArrayExamples.reversed(input1));
   }
 }
